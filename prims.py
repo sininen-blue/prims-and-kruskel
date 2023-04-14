5bc379c987ca4e945d9ad80f7cb0077d9d5bbea6
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2023-04-14 08:23:08 +0000

    setup test and plans for implementation

--- prims.py
+++ prims.py
@@ -3,11 +3,16 @@ import random
 def prims(graph):
     # select a random starting point (row?)
     # TODO setup an array with all the vertex currently connected/adjacent
+    # TODO setup an array for all edges
 
     starting_vertex = random.randint(0, len(graph) - 1)
-    
+
+    # lmao pretty sure thiw won't work for now, rework this entirely
+    print(starting_vertex + 1)
     for edge in graph[starting_vertex]:
-        print(edge)
+        if edge != 0:
+            # need to loop through this, because it skips numbers
+            print("this is vertex that is connected is", graph[starting_vertex], "and it's weights are", edge)
 
 
 input_graph = [

f475041f6bfb506214a53a586bb5fcc6e419896e
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2023-04-14 08:14:49 +0000

    added partial initial implementation of prims

--- prims.py
+++ prims.py
@@ -1,11 +1,19 @@
+import random
+
 def prims(graph):
-    print(len(graph))
+    # select a random starting point (row?)
+    # TODO setup an array with all the vertex currently connected/adjacent
+
+    starting_vertex = random.randint(0, len(graph) - 1)
+    
+    for edge in graph[starting_vertex]:
+        print(edge)
 
 
 input_graph = [
-    [0, 0, 0, 3],
-    [4, 0, 0, 3],
-    [0, 1, 0, 0],
+    [0, 1, 0, 3], 
+    [1, 0, 2, 3], 
+    [0, 2, 0, 0], 
     [3, 3, 0, 0]]
 
 prims(input_graph)

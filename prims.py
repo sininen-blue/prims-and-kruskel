2ed70ecc3d0c91b81f5b2707b1d90d28bda6bc27
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2023-04-14 12:26:16 +0000

    setup vertex choosing

--- prims.py
+++ prims.py
@@ -5,14 +5,25 @@ def prims(graph):
     # TODO setup an array with all the vertex currently connected/adjacent
     # TODO setup an array for all edges
 
-    starting_vertex = random.randint(0, len(graph) - 1)
+    connected_vertices = []
+    connected_vertices.append(random.randint(0, len(graph) - 1)) #random starting point
 
     # lmao pretty sure thiw won't work for now, rework this entirely
-    print(starting_vertex + 1)
-    for edge in graph[starting_vertex]:
-        if edge != 0:
-            # need to loop through this, because it skips numbers
-            print("this is vertex that is connected is", graph[starting_vertex], "and it's weights are", edge)
+    for vertex in connected_vertices:
+        print(graph[vertex])
+        
+        current_minimum_distance = 9999
+        vertex_to_add = 0
+        for edge in graph[vertex]:
+            if edge < current_minimum_distance and edge != 0:
+                current_minimum_distance = edge
+                vertex_to_add = graph[vertex].index(edge) 
+                # if there are multiple vertices that have the same ewight, it can't differentiate, but that works in this scenario\
+                
+                # TODO figure out which edge is chose here to repersent the graph
+                # TODO also figure out how to present output
+        
+        connected_vertices.append(vertex_to_add)
 
 
 input_graph = [

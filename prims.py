1ab1811d334b327da9259d7d0b6fc820127d1e2a
sininen-blue <alaanneil@gmail.com>
2023-04-15 11:59:25 +0800

    gave up, following tutorial

--- prims.py
+++ prims.py
@@ -1,31 +1,24 @@
 import random
 
 def prims(graph):
-    # select a random starting point (row?)
-    # TODO setup an array with all the vertex currently connected/adjacent
-    # TODO setup an array for all edges
+    number_of_vertices = len(graph)
 
     connected_vertices = []
     connected_vertices.append(random.randint(0, len(graph) - 1)) #random starting point
 
-    # lmao pretty sure thiw won't work for now, rework this entirely
-    for vertex in connected_vertices:
-        print(graph[vertex])
-        
-        current_minimum_distance = 9999
-        vertex_to_add = 0
-        for edge in graph[vertex]:
-            if edge < current_minimum_distance and edge != 0:
-                current_minimum_distance = edge
-                vertex_to_add = graph[vertex].index(edge) 
-                # if there are multiple vertices that have the same ewight, it can't differentiate, but that works in this scenario\
-                
-                # TODO figure out which edge is chose here to repersent the graph
-                # TODO also figure out how to present output
-        
-        connected_vertices.append(vertex_to_add)
+    used_edges = 0
+
+    while (used_edges < number_of_vertices - 1):
+        lowest_weight = 9999
+
+        # for all the connected vertices
+        # check for all the connected edges on that vertex
+        # check if that vertex is already connected
+        # get the minimum weight for that vertex
+        # pick that vertex, add it to connected, and then add that edge somewhere
 
 
+    
 input_graph = [
     [0, 1, 0, 3], 
     [1, 0, 2, 3], 

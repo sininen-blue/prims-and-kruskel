b21716c972ece4b6e3c32c6cf7d0f5568ee3e267
sininen-blue <alaanneil@gmail.com>
2023-04-15 13:21:06 +0800

    initial implementation, seems to work, needs testing

--- prims.py
+++ prims.py
@@ -4,18 +4,33 @@ def prims(graph):
     number_of_vertices = len(graph)
 
     connected_vertices = []
-    connected_vertices.append(random.randint(0, len(graph) - 1)) #random starting point
+    connected_vertices.append(0)
+    # connected_vertices.append(random.randint(0, len(graph) - 1)) #random starting point
 
-    used_edges = 0
-
-    while (used_edges < number_of_vertices - 1):
+    while (len(connected_vertices) < number_of_vertices):
         lowest_weight = 9999
+        origin_point = 0
+        vertex_to_add = 0
 
         # for all the connected vertices
-        # check for all the connected edges on that vertex
-        # check if that vertex is already connected
-        # get the minimum weight for that vertex
-        # pick that vertex, add it to connected, and then add that edge somewhere
+        for vertex_index in connected_vertices:
+            for edge_index in range(len(graph[vertex_index])):
+                if (graph[vertex_index][edge_index] != 0 and
+                    edge_index not in connected_vertices): #edge index == connected vertex index
+                    
+                    if graph[vertex_index][edge_index] < lowest_weight:
+                        lowest_weight = graph[vertex_index][edge_index]
+
+                        vertex_to_add = edge_index
+                        origin_point = vertex_index
+            
+        
+        connected_vertices.append(vertex_to_add)
+        print(f"connected vertex: {origin_point} to vertex:{vertex_to_add}, with the edge weight of {lowest_weight}")
+
+        # check for all the connected edges on that vertex_index and tthat they aren't already connected
+        # get the minimum weight for that vertex_index
+        # pick that vertex_index, add it to connected, and then add that edge somewhere
 
 
     
